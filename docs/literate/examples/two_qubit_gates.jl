8907283d6991600f75819516a87413f36eba1bfa
Gennadi Ryan <gennadiryan@gmail.com>
2025-07-07 01:05:19 -0400

    Removing plotting examples (until PiccoloPlots compatibility fixed)

--- docs/literate/examples/two_qubit_gates.jl
+++ docs/literate/examples/two_qubit_gates.jl
@@ -34,8 +34,8 @@ using PiccoloQuantumObjects
 using NamedTrajectories
 using LinearAlgebra
 
-using PiccoloPlots
-using CairoMakie
+# `using PiccoloPlots`
+# `using CairoMakie`
 
 ⊗(a, b) = kron(a, b)
 
@@ -115,8 +115,8 @@ println(fid_final)
 
 # Looks good!
 
-# Now let's plot the pulse and the population trajectories for the first two columns of the unitary, i.e. initial state of $\ket{00}$ and $\ket{01}$. For this we provide the function [`plot_unitary_populations`](@ref).
-plot_unitary_populations(prob.trajectory)
+# # Now let's plot the pulse and the population trajectories for the first two columns of the unitary, i.e. initial state of $\ket{00}$ and $\ket{01}$. For this we provide the function [`plot_unitary_populations`](@ref).
+# `plot_unitary_populations(prob.trajectory)`
 
 
 # For fun, let's look at a minimum time pulse for this problem
@@ -125,8 +125,8 @@ solve!(min_time_prob; max_iter=300)
 fid_final_min_time = unitary_rollout_fidelity(min_time_prob.trajectory, sys)
 println(fid_final_min_time)
 
-# And let's plot this solution
-plot_unitary_populations(min_time_prob.trajectory)
+# # And let's plot this solution
+# `plot_unitary_populations(min_time_prob.trajectory)`
 
 # It looks like our pulse derivative bounds are holding back the solution, but regardless, the duration has decreased:
 duration = get_duration(prob.trajectory)
@@ -182,9 +182,8 @@ println(fid_final)
 
 # Again, looks good!
 
-# Now let's plot the pulse and the population trajectories for the first two columns of the unitary, i.e. initial state of $\ket{00}$ and $\ket{01}$.
-
-plot_unitary_populations(prob.trajectory)
+# # Now let's plot the pulse and the population trajectories for the first two columns of the unitary, i.e. initial state of $\ket{00}$ and $\ket{01}$.
+# `plot_unitary_populations(prob.trajectory)`
 
 # For fun, let's look at a minimum time pulse for this problem
 
@@ -193,9 +192,8 @@ solve!(min_time_prob; max_iter=300)
 fid_final_min_time = unitary_rollout_fidelity(min_time_prob.trajectory, sys)
 println(fid_final_min_time)
 
-# And let's plot this solution
-
-plot_unitary_populations(min_time_prob.trajectory)
+# # And let's plot this solution
+# `plot_unitary_populations(min_time_prob.trajectory)`
 
 # It looks like our pulse derivative bounds are holding back the solution, but regardless, the duration has decreased:
 
